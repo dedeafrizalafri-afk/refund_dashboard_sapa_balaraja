953bcd0d5a300cc0e33f1aacdd16fe21e1b8c9cc
dedeafrizalafri-afk <dedeafrizalafri@gmail.com>
2026-08-02 11:38:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const data = {
-  pending: 12,
-  refund: 8,
-  selesai: 20
+    pending: 12,
+    refund: 8,
+    qty: 20,
+    toko: 5
 };
 
 document.getElementById("pending").textContent = data.pending;
 document.getElementById("refund").textContent = data.refund;
-document.getElementById("selesai").textContent = data.selesai;
+document.getElementById("qty").textContent = data.qty;
+document.getElementById("toko").textContent = data.toko;
