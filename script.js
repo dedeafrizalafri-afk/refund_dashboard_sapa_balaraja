6befaac7303b7a6dd9a71544a07b132296712565
dedeafrizalafri-afk <dedeafrizalafri@gmail.com>
2026-08-02 20:44:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
 
-document.getElementById("filterSampai").addEventListener("change", filterData);
-
-document.getElementById("filterToko").addEventListener("change", filterData);
-
-document.getElementById("filterStatus").addEventListener("change", filterData);
-
-// =====================================
-// MENU DASHBOARD
-// =====================================
-
-const btnDashboard = document.getElementById("btnDashboard");
-
-const btnData = document.getElementById("btnData");
-
-btnDashboard.addEventListener("click", function () {
-
-    document.getElementById("dashboardPage").style.display = "block";
-
-    document.getElementById("dataPage").style.display = "none";
-
-    btnDashboard.classList.add("active");
-
-    btnData.classList.remove("active");
-
-});
-
-btnData.addEventListener("click", function () {
-
-    document.getElementById("dashboardPage").style.display = "none";
-
-    document.getElementById("dataPage").style.display = "block";
-
-    btnData.classList.add("active");
-
-    btnDashboard.classList.remove("active");
-
 });
 // =====================================
 // AUTO REFRESH
