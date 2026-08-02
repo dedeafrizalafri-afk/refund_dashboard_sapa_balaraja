e41dc2767e3b6bc00b9100b099e8ffa32e54f98b
dedeafrizalafri-afk <dedeafrizalafri@gmail.com>
2026-08-02 11:30:29 +0700

    Add data object and update DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const data = {
+  pending: 12,
+  refund: 8,
+  selesai: 20
+};
 
+document.getElementById("pending").textContent = data.pending;
+document.getElementById("refund").textContent = data.refund;
+document.getElementById("selesai").textContent = data.selesai;
